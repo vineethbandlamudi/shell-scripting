5537fac97ce97821436b4747b7590c4983eaae12
vineeth <vineethbandlamudi@gmail.com>
2023-02-25 18:48:47 +0530

    Added Catalogue file

---
+++ catalogue.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+source common.sh
+
+PRINT "Install NodeJS\t"
+yum install nodejs make gcc-c++ -y  &>>$LOG
+STAT_CHECK $?
+
+PRINT "Add Roboshop User"
+id roboshop
+if [ $? -ne 0 ]; then
+  useradd roboshop
+fi
+STAT_CHECK $?
+
+PRINT "Download Application Code"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
+STAT_CHECK $?
+
+PRINT "Extract Application Code"
+cd /home/roboshop && unzip -o /tmp/catalogue.zip
+STAT_CHECK $?
