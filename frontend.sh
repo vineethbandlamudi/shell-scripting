43c06567b30f5f3ef392b307f24603cc96415763
vineeth <vineethbandlamudi@gmail.com>
2023-02-25 16:02:15 +0530

    Added Frontend and common script files

--- frontend.sh
+++ frontend.sh
@@ -8,19 +8,19 @@ STAT_CHECK $?
 
 PRINT "Download Frontend"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
-STAT_CHECK $1
+STAT_CHECK $?
 
 PRINT "Remove old HtDocs"
 cd /usr/share/nginx/html &>>$LOG && rm -rf * &>>$LOG
-STAT_CHECK $1
+STAT_CHECK $?
 
 PRINT "Extract Frontend Archive"
 unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* . &>>$LOG && mv static/* . &>>$LOG && rm -rf frontend-master static &>>$LOG
-STAT_CHECK $1
+STAT_CHECK $?
 
 PRINT "Updarte Roboshop Config"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
-STAT_CHECK $1
+STAT_CHECK $?
 
 PRINT "Enable Nginx"
 systemctl enable nginx &>>$LOG
