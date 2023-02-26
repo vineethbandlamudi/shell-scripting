793c874e998ada6cec6389d33905e76ee0ba8705
vineeth <vineethbandlamudi@gmail.com>
2023-02-26 10:21:38 +0530

    Added Redis file

--- redis.sh
+++ redis.sh
@@ -15,5 +15,5 @@ sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
 STAT_CHECK $?
 
 PRINT "Start Redis Service"
-systemctl enable redis &>>$LOG && systemctl enable redis &>>$LOG
+systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
 STAT_CHECK $?
