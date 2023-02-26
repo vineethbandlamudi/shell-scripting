023c8d71687b85c3e1ff51251f9971cfc0de880a
vineeth <vineethbandlamudi@gmail.com>
2023-02-26 10:11:23 +0530

    Added Redis file

---
+++ redis.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+source common.sh
+
+PRINT "Install Redis\t"
+yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$LOG
+STAT_CHECK $?
+
+PRINT "Install Redis\t"
+yum install redis -y enablerepo=remi &>>$LOG
+STAT_CHECK $?
+
+PRINT "Update Listen Address"
+sed 's/127.0.0.1/0.0.0.0' /etc/redis.conf /etc/redis/redis.co
+STAT_CHECK $?
+
+PRINT "Start Redis Service"
+systemctl enable redis &>>$LOG && systemctl enable redis &>>$LOG
+STAT_CHECK $?
