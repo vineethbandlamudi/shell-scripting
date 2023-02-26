7f1f9daefdb2f20958dfa192dbdb5b940337927c
vineeth <vineethbandlamudi@gmail.com>
2023-02-26 14:28:00 +0530

    Added RabbitMQ File

--- rabbimq.sh
+++ rabbimq.sh
@@ -3,6 +3,26 @@
 source common.sh
 
 PRINT "Install Erlang"
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>>$LOG
+if [  $? -ne 0 ]; then
+  yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>>$LOG
+fi
 STAT_CHECK $?
+
+PRINT "Setup RabbitMQ Repo"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>$LOG
+STAT_CHECK $?
+
+PRINT "Install RabbitMQ"
+yum install rabbitmq-server -y  &>>$LOG
+STAT_CHECK $?
+
+PRINT "Start RabbitMQ Service"
+systemctl enable rabbitmq-server &>>$LOG &&  systemctl start rabbitmq-server &>>$LOG
+STAT_CHECK $?
+
+PRINT "Create Applcation User"
+rabbitmqctl add_user roboshop roboshop123 &>>$LOG
+STAT_CHECK $?
+
+
 
